49e598e36c747edade080b1aa290022848a9b550
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-05-14 20:28:22 +0900

    テーブル内容取得

--- edit.php
+++ edit.php
@@ -11,7 +11,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);//結果の取得
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1 a.0">
     <title>Document</title>
 </head>
 <body>

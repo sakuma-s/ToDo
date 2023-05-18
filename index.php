9682eb44a9f16903f61fa39dee183117658441dc
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-05-18 13:34:19 +0900

    修正前に戻す

--- index.php
+++ index.php
@@ -5,6 +5,30 @@ require 'db_connection.php';
 function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+//GETで現在のページ数を取得する（未入力の場合は1を挿入）
+if (isset($_GET['page'])) {
+  $page = (int)$_GET['page'];
+} else {
+  $page = 1;
+}
+var_dump($page);
+//スタート
+if($page > 1) {
+  $start = ($page * 5) - 5;
+} else {
+  $start = 0;
+}
+
+$stmt = $pdo->prepare("SELECT * FROM list ORDER BY id DESC LIMIT 0,5");
+$stmt->execute();
+
+if (!$stmt) {
+  die($pdo->$error);
+}
+// $page = filter_input(INPUT_GET, 'page',FILTER_SANITIZE_NUMBER_INT);
+// $start = ($page - 1) * 5;
+// //データの取得
+// $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -26,20 +50,21 @@ function h($str) {
       </textarea>
       <p><input type="submit" name="btn_record" value="登録する"></p>
     </div>
-    <table>
-      <?php $results = $stmt->fetchAll(); ?>
-      <?php foreach ($results as $row) : ?>
-      <tr>
-        <tb><?php echo h($row['todolist']) . "<br>"; ?></tb>
-        <tb><a href="edit.php?id=<?php echo h($row['id']); ?>">編集</a></tb>
-      </tr>
-      <tr>
-      <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-      <input type="submit" name="delete" value="削除">
-      </tr>
-      </form>
+      <table>
+        <?php $results = $stmt->fetchAll(); ?>
+        <?php foreach ($results as $row) : ?>
+        <tr>
+          <tb><?php echo h($row['todolist']) . "<br>"; ?></tb>
+          <tb><a href="edit.php?id=<?php echo h($row['id']); ?>">編集</a></tb>
+        </tr>
+        <tr>
+        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+        <input type="submit" name="delete" value="削除">
+        </tr>
+        </form>
         <?php endforeach; ?>
-      </table>
+        </table>
+
     <!-- <script>
       var today = new Date();
       var todayHtml = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();

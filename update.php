f761db0e2969f86297c6757cf38893d52aff81b2
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-06-02 12:25:42 +0900

    例外処理の追加

--- update.php
+++ update.php
@@ -1,16 +1,21 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 require 'db_connection.php';
 
 if (isset($_POST['btn_update'])) {
     $id = $_POST['id'];
     $todolist = $_POST['todolist'];
-
+  try{
     $sql = 'UPDATE list SET todolist = :todolist WHERE id = :id';
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':todolist', $todolist, PDO::PARAM_STR);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-
+    $stmt->execute(array(':todolist' => $todolist,':id' => $id));
     header('Location: index.php');
     exit;
+
+  } catch (PDOexception $e) {
+    echo 'エラーが発生しました' . $e->getMessage();
+  }
 }

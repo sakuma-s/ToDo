97f2262b818e6ee273ecdc977b50727c96c575fc
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-06-02 19:29:42 +0900

    受け取る値の変更

--- update.php
+++ update.php
@@ -3,19 +3,20 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 require 'db_connection.php';
 
-if (isset($_POST['btn_update'])) {
-    $id = $_POST['id'];
-    $todolist = $_POST['todolist'];
+if (isset($_POST['edit'])) {
+  $id = $_POST['id'];
+  $edit = $_POST['edit'];
+
   try{
     $sql = 'UPDATE list SET todolist = :todolist WHERE id = :id';
     $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':todolist', $todolist, PDO::PARAM_STR);
+    $stmt->bindParam(':todolist', $edit, PDO::PARAM_STR);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute(array(':todolist' => $todolist,':id' => $id));
+    $stmt->execute();
     header('Location: index.php');
     exit;
-
-  } catch (PDOexception $e) {
+  } catch (PDOException $e) {
     echo 'エラーが発生しました' . $e->getMessage();
   }
 }
+?>
